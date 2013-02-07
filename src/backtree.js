3ac1764d5cefcc07d1762d04312243b07a958dbc
Tim Child <tim@cantemo.com>
2013-02-07 20:56:05 +0100

    Some clean up to the styles

--- src/backtree.js
+++ src/backtree.js
@@ -2,8 +2,10 @@ var root = window;
 
 backtree = root.Backtree = {
   VERSION: "0.0.1",
-}
 
+  // check if we are using a mobile
+  isMobile: (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent))
+}
 backtree.TreeView = Backbone.View.extend({
   /*  Main Tree View 
    *  Represents a tree.
@@ -12,6 +14,7 @@ backtree.TreeView = Backbone.View.extend({
    *  Optional el selector to bind to a route element.
    *  Optionally it takes a class name otherwise that defaults to 'backtree'.
    *  Optional 'StateAttribute': Attribute used on element for closed or open branch.
+   *  Optional template attribute: function call for rendering templates.
    *
    *  var tree = new Backtree.Tree({
    *    root: mybootstrappedcollection,
@@ -30,6 +33,7 @@ backtree.TreeView = Backbone.View.extend({
     this.children = new Backbone.ChildViewContainer();
     var args = Array.prototype.slice.apply(arguments);
     Backbone.View.prototype.constructor.apply(this, args);
+  
 
     // Bind the initial events ala Marionette.
     this._initialEvents();
@@ -38,7 +42,7 @@ backtree.TreeView = Backbone.View.extend({
   _initialEvents: function(){
     if (this.collection){
       this.listenTo(this.collection, "add", this.addChildView, this);
-      this.listenTo(this.collection, "remove", this.removeItemView, this);
+      this.listenTo(this.collection, "remove", this.removeNodeView, this);
       this.listenTo(this.collection, "reset", this.render, this);
     }
   },
@@ -48,12 +52,16 @@ backtree.TreeView = Backbone.View.extend({
     this.stateAttribute = options.stateAttribute || "state";
     this.branchAttribute = options.branchAttribute || "contents";
     this.collection = options.collection;
+    this.childTemplateRenderer = options.childTemplateRenderer || undefined;
 
     this.$el.empty().addClass(this.className).attr('role', 'navigation');
     // If we pass in an element then we can render on to it.
     if (options.el !== undefined){
       this.render();
     };
+  },
+
+  events: function() {
   },
 
   render: function(){
@@ -67,6 +75,7 @@ backtree.TreeView = Backbone.View.extend({
     return this;
   },
 
+
   /* Build up a view of the nodes in the collection. */
   buildCollectionView: function(){
     var self = this;
@@ -77,9 +86,20 @@ backtree.TreeView = Backbone.View.extend({
     });
   },
 
-  /* addItemView */
+  /* Add a child view to the DOM */
+  addChildView: function(item, collection, options){
+    var index = this.collection.indexOf(item);
+    this.addNodeView(item, backtree.NodeView, index);
+  },
+
+  /* Add a node */
   addNodeView: function(node, NodeViewType, index){
-    var view = new NodeViewType({model: node, parentView:this, branchAttribute: this.branchAttribute});
+    var view = new NodeViewType({
+        model: node, 
+        parentView: this, 
+        branchAttribute: this.branchAttribute,
+        templateRenderer: this.childTemplateRenderer 
+    });
 
     // Keep track of childviews & render it
     this.children.add(view);
@@ -94,6 +114,11 @@ backtree.TreeView = Backbone.View.extend({
     collectionView.$el.append(nodeView.el);
   },
 
+  removeNodeView: function(node){ 
+    view = this.children.findByModel(node);
+    this.removeChildView(view);
+  },
+
   /* Remove a single child view */
   removeChildView: function(view){
     if (view){
@@ -102,13 +127,12 @@ backtree.TreeView = Backbone.View.extend({
       }
       this.children.remove(view);
     }
-
   },
 
-  /* Add a child view to the DOM */
-  addChildView: function(item, collection, options){
-    var index = this.collection.indexOf(item);
-    this.addNodeView(item, backtree.NodeView, index);
+  // Another good idea inspired by Marionette
+  close: function(){
+    this.closeChildren();
+    this.remove();
   },
 
   /* Close the child views that this collection view
@@ -125,9 +149,10 @@ backtree.NodeView = backtree.TreeView.extend({
    * 
    * */
   tagName: "ul",
-  template: _.template('<li><%= name||id %></li>'),
-  events: {
-    "click: ": "openHandler"
+  template: _.template('<li><div class="bt-node"><div class="bt-type-<%= type %>"></div><div class="bt-icon bt-icon-<%= type %>"></div><%= name||id %></div></li>'),
+  //template: _.template('<li><%= name||id %></li>'),
+  events: function(){
+    return backtree.isMobile ? { "touchstart": 'select' } : { "click .bt-node": 'select'}
   },
   constructor: function(options){
     var args = Array.prototype.slice.apply(arguments);
@@ -136,11 +161,12 @@ backtree.NodeView = backtree.TreeView.extend({
     backtree.TreeView.apply(this, args);
   },
   initialize: function(options){
+    this.templateRenderer = options.templateRenderer;
     this.collection = this.model[options.branchAttribute];
   },
   render: function(){
-    var html = this.template(this.model.attributes);
-    this.$el.html(html).attr('data-id', this.model.cid);
+    var html = this.returnRendered();
+    this.$el.addClass('backtreespacer').html(html).attr('data-id', this.model.cid);
     this.renderCollection();
     return this;
   },
@@ -148,8 +174,28 @@ backtree.NodeView = backtree.TreeView.extend({
     var args = Array.prototype.slice.apply(arguments);
     backtree.TreeView.prototype.render.apply(this, args);
   },
+  returnRendered: function(){
+    var html = '';
+    if(this.templateRenderer !== undefined){
+      html = this.templateRenderer(this.model, this);
+    } else {
+      html = this.template(this.model.attributes);
+    }
+    return html;
+  },
   appendHTML: function(collectionView, nodeView){
-    collectionView.$("li:first").append(nodeView.el);
+    collectionView.$("li:first")
+        .append(nodeView.el)
+        .find("div.bt-type-collection:first")
+        .addClass("bt-type-collection-open")
+        .end()
+        .find("div.bt-icon:first")
+        .addClass("bt-icon-collection-open");
+  },
+  select: function(event){
+    event.preventDefault();
+    event.stopPropagation();
+    this.$('.bt-node:first').toggleClass('bt-node-selected');
   },
   openHandler: function(event){
     
--- test/spec/BacktreeSpec.js
+++ test/spec/BacktreeSpec.js
@@ -1,4 +1,28 @@
-describe("Backtree", function() {
+/*  Test scripts depend on Jasmine and Jasmine.JQuery */
+describe("Custom Template Renderer", function(){
+  beforeEach(function() {
+    myCollection = new testCollection(jsonstructure.contents);
+    tree = new Backtree.TreeView({
+        collection: myCollection,
+        el: '#backtree',
+        className: 'backtree',
+        childTemplateRenderer: function(model, view){
+          return ('<div>' + model.get('id') + '</div>');
+        } 
+    });
+  });
+  it("should be defined correctly", function(){
+    expect(tree).toBeDefined();
+    expect(tree.children).toBeDefined();
+  });
+  it("should use the custom renderer function to return HTML", function(){
+    expect(tree.$el).toContainHtml('<div>collection1</div>');
+    expect(tree.$el).toContainHtml('<div>collection2</div>');
+    expect(tree.$el).toContainHtml('<div>collection3</div>');
+  });
+});
+
+describe("Basic Backtree Usage", function() {
   /* var myCollection, tree; */
 
   beforeEach(function() {
@@ -10,23 +34,29 @@ describe("Backtree", function() {
     });
   });
 
-  it("should be defined", function() {
+  it("should be define a view with children views", function() {
     expect(tree).toBeDefined();
+    expect(tree.children).toBeDefined();
   });
+
   it("should remove everything under the existing dom element.", function() {
     expect(tree.$el.html()).toNotBe('Test');
   });
-  it("should get the class from the initalisation.", function() {
+
+  it("should get the class from the initialization.", function() {
     expect(tree.$el.hasClass(tree.className)).toBe(true);
   });
+
   it("should have created the data structure in the view.", function() {
     expect(tree.collection).toBeDefined();
     expect(tree.collection.get('collection1')).not.toBeUndefined();
   });
+
   it("should append the structure to the DOM correctly", function(){
-    expect(tree.$el).toContainHtml('collection1');
-    expect(tree.$el).toContainHtml('subcollection1');
+    expect(tree.$el).toContainHtml('Collection 3');
+    expect(tree.$el).toContainHtml('A Sub Collection');
   });
+
   it("should add new childviews to the DOM correctly", function(){
     // This adds a relatively easy amendment.
     tree.collection.add([new testModel({
@@ -43,9 +73,40 @@ describe("Backtree", function() {
     expect(tree.$el).toContainHtml('testAddNewNode');
     expect(tree.$el).toContainHtml('testAddNewSubCollection');
   });
+
+
+  it("should be possible to remove child nodes as needed", function(){
+    tree.collection.add([new testModel({
+      id: 'removeMe',
+      name:'removeMe', 
+      type:'collection',
+      url:'/test',
+      contents: [{
+        id: 'subcollection1',
+        name: 'removeSub'
+      }]
+      })
+    ]);
+    tree.collection.remove(tree.collection.get('removeMe'));
+    expect(tree.$el).not.toContainHtml('removeSub');
+    expect(tree.$el).not.toContainHtml('removeMe');
+  });
+
+  it("should be possible to update the name of a childnode and it updates in the DOM", function(){
+
+  });
+
+  it("should be possible to handle clicks", function(){
+
+  });
+
   it("should be able to put something a little deeper", function(){
     tree.collection.models[2].contents.add([new testModel({
       id: 'testDeeper',
+      name: 'Test Addition',
+      type: 'collection'
     })]); 
+    expect(tree.$el).toContainHtml('Test Addition');
   });
+
 });
